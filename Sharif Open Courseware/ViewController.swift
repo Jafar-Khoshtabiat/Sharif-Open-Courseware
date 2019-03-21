69248d283458004153adff5e4e84203ecf9d0f8a
Jafar Khoshtabiat <jafar.khoshtabiat@gmail.com>
2019-03-21 04:24:54 +0330

    working on "Course" page: top and bottom bar views almost done

--- Sharif Open Courseware/ViewController.swift
+++ Sharif Open Courseware/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(UIApplication.shared.statusBarFrame.height)
     }
-
-
 }
 
