19f4e6e4d425d4defd40ed3436272784ea6e28f6
Jafar Khoshtabiat <jafar.khoshtabiat@gmail.com>
2019-04-03 12:22:46 +0430

    showing mock data in DepartmentsCollectionView in HomeViewController

--- Sharif Open Courseware/Views/DepartmentCollectionViewCell/DepartmentCollectionViewCell.swift
+++ Sharif Open Courseware/Views/DepartmentCollectionViewCell/DepartmentCollectionViewCell.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class DepartmentCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var roundedView: UIView!
+    @IBOutlet weak var departmentImageView: UIImageView!
+    @IBOutlet weak var departmentNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.roundedView.layer.cornerRadius = 8
+        
+        self.layer.applySketchShadow(color: UIColor(named: "slate")!, alpha: 0.22, x: 0, y: 5, blur: 13, spread: 0)
+        self.layer.masksToBounds = false
+        
+        self.departmentImageView.layer.cornerRadius = self.departmentImageView.frame.height / 2
     }
 }
