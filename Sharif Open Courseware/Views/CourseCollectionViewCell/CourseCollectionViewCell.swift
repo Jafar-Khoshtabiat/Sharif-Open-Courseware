65b1946f165bd3cfba2a1fa4545986a7ed784751
Jafar Khoshtabiat <jafar.khoshtabiat@gmail.com>
2019-04-03 14:07:19 +0430

    working on CourseCollectionView in HomeViewController

--- Sharif Open Courseware/Views/CourseCollectionViewCell/CourseCollectionViewCell.swift
+++ Sharif Open Courseware/Views/CourseCollectionViewCell/CourseCollectionViewCell.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class CourseCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var roundedView: UIView!
+    @IBOutlet weak var courseImageView: UIImageView!
+    @IBOutlet weak var courseNameLabel: UILabel!
+    @IBOutlet weak var courseTeacherNameLabel: UILabel!
+    @IBOutlet weak var courseDepartmentNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        self.roundedView.layer.cornerRadius = 8
+        self.roundedView.layer.masksToBounds = true
+        
+        self.layer.applySketchShadow(color: UIColor(named: "slate")!, alpha: 0.14, x: 0, y: 8, blur: 14, spread: 0)
+        self.layer.masksToBounds = false
     }
 }
