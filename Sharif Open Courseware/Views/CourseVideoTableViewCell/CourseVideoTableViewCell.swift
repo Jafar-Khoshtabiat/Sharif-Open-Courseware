a13cefaa2bc9e8d728e6d0c23158d507a28a63a7
Jafar Khoshtabiat <jafar.khoshtabiat@gmail.com>
2019-04-07 21:04:22 +0430

    going to implement CourseVideoTableViewCell

---
+++ Sharif Open Courseware/Views/CourseVideoTableViewCell/CourseVideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CourseVideoTableViewCell.swift
+//  Sharif Open Courseware
+//
+//  Created by Jafar Khoshtabiat on 4/7/19.
+//  Copyright © 2019 Sharif University of Technology. All rights reserved.
+//
+
+import UIKit
+
+class CourseVideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
